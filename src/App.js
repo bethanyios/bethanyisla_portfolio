e11b56f2110bcbf234d3b67b7b2db95018c57af2
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-22 23:01:28 +0000

    Create expanded bubble shapes and amend architecture for bubbles component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 // import plantbg from './Components/plantbg.jpg';
-// import Bubble from './Components/bubbles';
+import Bubbles from './Components/Bubbles.js';
 import './App.css';
-import './bubbles.css';
+import './Components/bubbles.css';
 
 
 function App() {
@@ -10,22 +10,7 @@ function App() {
       <div>
       <header className="Font">
       </header>
-
-      <section className="bubbles">
-      <div className="circle-stack">
-      <span className="circle-stack-text"> Stack </span>
-      </div>
-      <div className="circle-profiles">
-      <span className="circle-profiles-text"> Profiles </span>
-      </div>
-      <div className="circle-experience">
-      <span className="circle-experience-text"> Experience </span>
-      </div>
-      <div className="circle-interests">
-      <span className="circle-interests-text"> Interests </span>
-      </div>
-      </section>
-
+      <Bubbles />
       </div>
   );
 }
---
+++ src/Components/Bubbles.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function Bubbles() {
+  return (
+<section className="bubbles">
+<div className="bubble">Stack
+<div className="rectangle"> </div>
+</div>
+
+<div className="bubble">Profiles
+<div className="rectangle"> </div>
+</div>
+
+<div className="bubble">Experience
+<div className="rectangle"></div>
+</div>
+
+<div className="bubble">Interests
+<div className="rectangle"> </div>
+</div>
+
+
+</section>
+);
+}
+
+export default Bubbles;
