c74e0730cbf249d07fbaf1de8d88572814be50b6
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-02-08 20:47:18 +0000

    Change flipcard design again and background colour for more depth

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -6,7 +6,8 @@ const Projects = () => {
     <>
       <h1> Projects </h1>
       <section className="project-content">
-      lorem ipsum yaddayadda
+      Project gallery:
+      <a target="_blank" href="https://fac18.github.io/fac18-projects-gallery/"> FAC cohort 18 </a>
       </section>
       </>
     )
