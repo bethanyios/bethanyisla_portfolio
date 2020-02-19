19928645accb5c39c8d7c82e1bbce3b6194baf33
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-02-19 21:25:41 +0000

    Add cursor to svg in index.css

--- src/Components/FlipCards/index.js
+++ src/Components/FlipCards/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { FlipCardStack, FlipCardExperience,  FlipCardInterests, FlipCardProfiles } from "./FlipCards.js";
-// import Navbar from "../Navbar/index.js";
 import "./style.css";
 
 const FlipCards = () => {
@@ -8,9 +7,9 @@ const FlipCards = () => {
   return (
     <section className="cards">
     <FlipCardStack />
-    <FlipCardProfiles/>
-    <FlipCardExperience/>
-    <FlipCardInterests/>
+    <FlipCardProfiles />
+    <FlipCardExperience />
+    <FlipCardInterests />
     </section>
   )
 }
